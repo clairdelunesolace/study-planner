8ff013129317d90a7fd1c30cf8f9c587f5ac4a0d
Shuai Yin <syin13@uci.edu>
2026-07-22 17:06:24 +0800

    Complete task CRUD APIs

--- src/main/java/com/shuaiyin/studyplanner/controller/TaskController.java
+++ src/main/java/com/shuaiyin/studyplanner/controller/TaskController.java
@@ -4,6 +4,7 @@ import com.shuaiyin.studyplanner.model.Task;
 import com.shuaiyin.studyplanner.repository.TaskRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -21,8 +22,44 @@ public class TaskController {
         return taskRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public Task getTaskById(@PathVariable Long id){
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
     @PostMapping
     public Task createTask(@RequestBody Task task){
         return taskRepository.save(task);
+    }
+
+    @PutMapping("/{id}")
+    public Task updateTask(
+            @PathVariable Long id,
+            @RequestBody Task updatedTask
+    ){
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+        task.setTitle(updatedTask.getTitle());
+        task.setDescription(updatedTask.getDescription());
+        task.setDeadline(updatedTask.getDeadLine());
+
+        return taskRepository.save(task);
+    }
+
+    @PatchMapping("/{id}/complete")
+    public Task completeTask(@PathVariable Long id){
+        Task task = taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+        task.setCompleted(true);
+        task.setCompletedAt(LocalDateTime.now());
+
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id){
+        taskRepository.deleteById(id);
     }
 }
